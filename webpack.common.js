934fbb31fe95e267e4e1afb0f5e1d0483123b60f
dongbin420 <dongbin420@gmail.com>
2024-06-21 17:11:36 +0900

    chore: favicon 삽입 방식 변경
    다양한 크기의 favicon 삽입을 위해

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -41,7 +42,19 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
       hash: true,
-      favicon: './public/favicon.ico',
+    }),
+    new FaviconsWebpackPlugin({
+      logo: './public/favicon.png',
+      favicons: {
+        icons: {
+          favicons: true,
+          android: false,
+          appleIcon: false,
+          appleStartup: false,
+          windows: false,
+          yandex: false,
+        },
+      },
     }),
     new CopyWebpackPlugin({
       patterns: [{ from: './public/og', to: './og' }],
