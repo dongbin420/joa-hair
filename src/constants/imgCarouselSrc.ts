71b756367a620c2a138cdb3491126a82053ecc0a
dongbin420 <dongbin420@gmail.com>
2024-06-24 19:43:38 +0900

    chore: 캐러셀 컴포넌트 예시 이미지 주소 변경

--- src/constants/imgCarouselSrc.ts
+++ src/constants/imgCarouselSrc.ts
@@ -1,5 +1,5 @@
 export const pepeImgCarouselSrc = [
-  'https://beincrypto.com/wp-content/uploads/2023/05/bic_Pepe_neutral_2-850x478.png.webp',
+  'https://kr.cryptonews.com/wp-content/uploads/sites/18/2023/10/1698248758-pepe-coin-rise-1024x536.webp',
   'https://static.news.bitcoin.com/wp-content/uploads/2023/04/pepes.jpg',
   'https://upload.wikimedia.org/wikipedia/en/thumb/6/63/Feels_good_man.jpg/200px-Feels_good_man.jpg',
   'https://www.tbstat.com/cdn-cgi/image/format=webp,q=75/wp/uploads/2023/05/Fvz9hOIXwAEaIR8.jpeg',
