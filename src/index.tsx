acc6bbea5d4ff445d7259b98b25204d248580380
dongbin420 <dongbin420@gmail.com>
2024-07-15 19:04:59 +0900

    refactor: lazy 로딩 구현
    메인페이지, 에러페이지를 제외한 페이지에 대해 레이지 로딩 구현

--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '@/styles/GlobalStyle';
 import theme from '@/styles/theme';
-import App from '@/App';
-import MainPage from '@/pages/MainPage/MainPage';
-import ErrorPage from '@/pages/ErrorPage/ErrorPage';
-import AboutPage from '@/pages/AboutPage/AboutPage';
-import ServicesPage from '@/pages/ServicesPage/ServicesPage';
-import GalleryPage from '@/pages/GalleryPage/GalleryPage';
-import ContactPage from '@/pages/ContactPage/ContactPage';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <MainPage />,
-      },
-      {
-        path: 'about',
-        element: <AboutPage />,
-      },
-      {
-        path: 'services',
-        element: <ServicesPage />,
-      },
-      {
-        path: 'gallery',
-        element: <GalleryPage />,
-      },
-      {
-        path: 'contact',
-        element: <ContactPage />,
-      },
-    ],
-  },
-]);
+import router from './router';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
